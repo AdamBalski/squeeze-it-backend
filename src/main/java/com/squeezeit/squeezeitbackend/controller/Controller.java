8eaa65a717211c7941d460afc30229f26b86a02a
AdamBalski <adambalski1@gmail.com>
2020-07-26 19:29:08 +0200

    Finished

--- src/main/java/com/squeezeit/squeezeitbackend/controller/Controller.java
+++ src/main/java/com/squeezeit/squeezeitbackend/controller/Controller.java
@@ -3,6 +3,7 @@ package com.squeezeit.squeezeitbackend.controller;
 import com.squeezeit.squeezeitbackend.dto.MinifiedDto;
 import com.squeezeit.squeezeitbackend.dto.OriginalDto;
 import com.squeezeit.squeezeitbackend.service.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
  * @author Adam Balski
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:8081")
 public class Controller {
     private final Service service;
 
